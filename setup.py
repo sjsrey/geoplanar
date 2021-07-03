179eef010968828da15405b4f90f334dc74c3c8e
Serge Rey <sjsrey2gmail.com>
2021-07-02 17:26:34 -0700

    License and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='geoplanar',
+      version='0.1',
+      description='Geographic planar enforcement of polygon geoseries',
+      author='Serge Rey',
+      author_email='sjsrey@gmail.com',
+      url='https://github.com/sjsrey/geoplanar',
+      packages=['geoplanar'],
+     )
