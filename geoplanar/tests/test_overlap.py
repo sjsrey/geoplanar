74b0e6a90eed74e2ead0fb9ef8c97b61e2e35b80
Serge Rey <sjsrey@gmail.com>
2021-07-05 11:35:33 -0700

    tests for overlap

--- geoplanar/tests/test_overlap.py
+++ geoplanar/tests/test_overlap.py
@@ -27,3 +27,18 @@ def test_trim_overlaps():
     gdf = geopandas.GeoDataFrame(geometry=[p1,p2])
     gdf1 = trim_overlaps(gdf, largest=False)
     assert_equal(gdf1.area.values, numpy.array([100.,4.]))
+
+
+    p1 = box(0,0,10,10)
+    p2 = box(10,0, 20,10)
+    p3 = box(8,4, 12,6)
+    gdf = geopandas.GeoDataFrame(geometry=[p1,p2,p3])
+    gdf1 = trim_overlaps(gdf, largest=False)
+    assert_equal(gdf1.area.values, numpy.array([100.,100., 0.]))
+
+    p1 = box(0,0,10,10)
+    p2 = box(10,0, 20,10)
+    p3 = box(8,4, 12,6)
+    gdf = geopandas.GeoDataFrame(geometry=[p1,p2,p3])
+    gdf1 = trim_overlaps(gdf)
+    assert_equal(gdf1.area.values, numpy.array([96.,96., 8.]))
