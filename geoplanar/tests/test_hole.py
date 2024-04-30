c873f63f1b2750d4ea767b3f9601b4892edfb102
Martin Fleischmann <martin@martinfleischmann.net>
2024-04-30 13:40:13 +0200

    TST: use proper pytest structure of tests

--- geoplanar/tests/test_hole.py
+++ geoplanar/tests/test_hole.py
@@ -6,16 +6,14 @@ from shapely.geometry import box
 from geoplanar.hole import missing_interiors
 
 
-def setup():
-    p1 = box(0, 0, 10, 10)
-    p2 = box(1, 1, 3, 3)
-    p3 = box(7, 7, 9, 9)
+class TestHole:
+    def setup_method(self):
+        p1 = box(0, 0, 10, 10)
+        p2 = box(1, 1, 3, 3)
+        p3 = box(7, 7, 9, 9)
 
-    gdf = geopandas.GeoDataFrame(geometry=[p1, p2, p3])
-    return gdf
+        self.gdf = geopandas.GeoDataFrame(geometry=[p1, p2, p3])
 
-
-def test_missing_interiors():
-    gdf = setup()
-    mi = missing_interiors(gdf)
-    assert mi == [(0, 1), (0, 2)]
+    def test_missing_interiors(self):
+        mi = missing_interiors(self.gdf)
+        assert mi == [(0, 1), (0, 2)]
--- geoplanar/tests/test_planar.py
+++ geoplanar/tests/test_planar.py
@@ -7,23 +7,21 @@ from shapely.geometry import MultiPolygon, Polygon
 import geoplanar
 
 
-def setup():
-    c1 = [[0, 0], [0, 10], [10, 10], [10, 0], [0, 0]]
-    p1 = Polygon(c1)
-    c2 = [[10, 2], [20, 8], [20, 2], [10, 2]]
-    p3 = Polygon([[21, 2], [21, 4], [23, 3]])
+class TestPlanar:
+    def setup_method(self):
+        c1 = [[0, 0], [0, 10], [10, 10], [10, 0], [0, 0]]
+        p1 = Polygon(c1)
+        c2 = [[10, 2], [20, 8], [20, 2], [10, 2]]
+        p3 = Polygon([[21, 2], [21, 4], [23, 3]])
 
-    # p2 = Polygon(c2)
-    p2 = MultiPolygon([Polygon(c2), p3])
+        p2 = MultiPolygon([Polygon(c2), p3])
 
-    gdf = geopandas.GeoDataFrame(geometry=[p1, p2])
+        self.gdf = geopandas.GeoDataFrame(geometry=[p1, p2])
 
-    return gdf
-
-
-def test_non_planar_edges():
-    gdf = setup()
-    res = geoplanar.non_planar_edges(gdf)
-    assert res.equals(Graph.from_dicts({0: [1], 1: [0]}))
-    gdf1 = geoplanar.fix_npe_edges(gdf)
-    assert_equal(gdf1.geometry[0].wkt, "POLYGON ((0 0, 0 10, 10 10, 10 2, 10 0, 0 0))")
+    def test_non_planar_edges(self):
+        res = geoplanar.non_planar_edges(self.gdf)
+        assert res.equals(Graph.from_dicts({0: [1], 1: [0]}))
+        gdf1 = geoplanar.fix_npe_edges(self.gdf)
+        assert_equal(
+            gdf1.geometry[0].wkt, "POLYGON ((0 0, 0 10, 10 10, 10 2, 10 0, 0 0))"
+        )
