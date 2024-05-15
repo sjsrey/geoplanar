5452f88596f0bea87e1397733726263cf5c5b919
Lisa <90904984+lisawink@users.noreply.github.com>
2024-05-15 16:04:29 +0200

    Add merge overlaps function (#50)
    * add merge_overlaps function
    
    * make sindex queries backwards compatible
    
    ---------
    
    Co-authored-by: Lisa <90904984+LisaOJWinkler@users.noreply.github.com>

--- geoplanar/overlap.py
+++ geoplanar/overlap.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import geopandas
+import libpysal
+import numpy as np
+from collections import defaultdict
 from packaging.version import Version
 
-__all__ = ["overlaps", "trim_overlaps", "is_overlapping"]
+__all__ = ["overlaps", "trim_overlaps", "is_overlapping", "merge_overlaps"]
 
 GPD_GE_014 = Version(geopandas.__version__) >= Version("0.14.0")
 
@@ -79,3 +82,67 @@ def is_overlapping(gdf):
     if overlaps.shape[1] > 0:
         return True
     return False
+
+def merge_overlaps(gdf, merge_limit, overlap_limit):
+    """Merge overlapping polygons based on a set of conditions.
+
+    Overlapping polygons smaller than ``merge_limit`` are merged to a neighboring polygon.
+
+    Polygons larger than ``merge_limit`` are merged to neighboring if they share area larger
+    than ``area * overlap_limit``.
+
+    Parameters
+    ----------
+    gdf : GeoDataFrame
+        GeoDataFrame with polygon or mutli polygon geometry
+    merge_limit : float
+        area of overlapping polygons that are to be merged with neighbors no matter the size
+        of the overlap
+    overlap_limit : float (0-1)
+        ratio of area of an overlapping polygon that has to be shared with other polygon
+        to merge both into one
+
+    Returns
+    -------
+
+    GeoDataFrame
+    """
+
+    if GPD_GE_014:
+        overlap_a, overlap_b = gdf.sindex.query(gdf.geometry, predicate="overlaps")
+        contains_a, contains_b = gdf.sindex.query(gdf.geometry, predicate="contains")
+    else:
+        overlap_a, overlap_b = gdf.sindex.query_bulk(gdf.geometry, predicate="overlaps")
+        contains_a, contains_b = gdf.sindex.query_bulk(gdf.geometry, predicate="contains")
+
+    self_mask = contains_a != contains_b
+    contains_a = contains_a[self_mask]
+    contains_b = contains_b[self_mask]
+
+    self_mask = overlap_a != overlap_b
+    overlap_a = overlap_a[self_mask]
+    overlap_b = overlap_b[self_mask]
+
+    source = np.concatenate([overlap_a, contains_a])
+    target = np.concatenate([overlap_b, contains_b])
+
+    neighbors = defaultdict(list)
+    for key, value in zip(source, target):
+        neighbors[key].append(value)
+
+    neighbors_final = {}
+
+    for i, poly in gdf.geometry.items():
+        if i in neighbors:
+            if poly.area < merge_limit:
+                neighbors_final[i] = neighbors[i]
+            else:
+                sub = gdf.geometry.iloc[neighbors[i]]
+                inters = sub.intersection(poly)
+                include = sub.index[inters.area > (sub.area * overlap_limit)]
+                neighbors_final[i] = list(include)
+        else:
+            neighbors_final[i]=[]
+
+    W = libpysal.weights.W(neighbors_final, silence_warnings=True)
+    return gdf.dissolve(W.component_labels)
