8d649f2fa433d6a85b10ff686130bb7bf354c0ca
Lisa Winkler <90904984+lisawink@users.noreply.github.com>
2024-05-17 10:53:08 +0200

    Add merge touching function (#51)
    * add merge touching function
    
    * update merge_touching documentation
    
    * make sindex query backwards compatible
    
    * fix indentation
    
    * add extra tests
    
    ---------
    
    Co-authored-by: Lisa <90904984+LisaOJWinkler@users.noreply.github.com>

--- geoplanar/overlap.py
+++ geoplanar/overlap.py
@@ -5,10 +5,9 @@ import numpy as np
 from collections import defaultdict
 from packaging.version import Version
 
-__all__ = ["overlaps", "trim_overlaps", "is_overlapping", "merge_overlaps"]
+__all__ = ["overlaps", "trim_overlaps", "is_overlapping", "merge_overlaps", "merge_touching"]
 
 GPD_GE_014 = Version(geopandas.__version__) >= Version("0.14.0")
-
 
 def overlaps(gdf):
     if GPD_GE_014:
@@ -94,7 +93,7 @@ def is_overlapping(gdf):
     if overlaps.shape[1] > 0:
         return True
     return False
-
+  
 def merge_overlaps(gdf, merge_limit, overlap_limit):
     """Merge overlapping polygons based on a set of conditions.
 
@@ -113,13 +112,12 @@ def merge_overlaps(gdf, merge_limit, overlap_limit):
     overlap_limit : float (0-1)
         ratio of area of an overlapping polygon that has to be shared with other polygon
         to merge both into one
-
+    
     Returns
     -------
 
     GeoDataFrame
     """
-
     if GPD_GE_014:
         overlap_a, overlap_b = gdf.sindex.query(gdf.geometry, predicate="overlaps")
         contains_a, contains_b = gdf.sindex.query(gdf.geometry, predicate="contains")
@@ -158,3 +156,58 @@ def merge_overlaps(gdf, merge_limit, overlap_limit):
 
     W = libpysal.weights.W(neighbors_final, silence_warnings=True)
     return gdf.dissolve(W.component_labels)
+
+def merge_touching(gdf, index, largest=None):
+    """Merge or remove polygons based on a set of conditions.
+    If polygon does not share any boundary with another polygon, remove. If it shares some boundary with
+    a neighbouring polygon, join to that polygon. If ``largest=None`` it picks one randomly, otherwise it 
+    picks the polygon with which it shares the largest (True) or the smallest (False) boundary.
+
+    Parameters
+    ----------
+    gdf : GeoDataFrame
+        GeoDataFrame with polygon or mutli polygon geometry
+    index : list of indexes
+        list of indexes of polygons in gdf to merge or remove
+    largest : bool (default None)
+        Merge with the polygon with the largest (True) or smallest (False) shared boundary.
+        If None, merge with any neighbor non-deterministically but performantly.
+
+    Returns
+    -------
+
+    GeoDataFrame
+    """
+    
+    merge_gdf = gdf.loc[index]
+
+    if GPD_GE_014:
+        source, target = gdf.boundary.sindex.query(merge_gdf.boundary, predicate="overlaps")
+    else:
+        source, target = gdf.boundary.sindex.query_bulk(merge_gdf.boundary, predicate="overlaps")
+
+    source = merge_gdf.index[source]
+    target = gdf.index[target]
+
+    neighbors={}
+    delete = []
+    for i, poly in gdf.geometry.items():
+        if i in merge_gdf.index:
+            if i in source:
+                if largest is None:
+                    neighbors[i]=[target[source==i][0]]
+                else:
+                    sub = gdf.geometry.loc[target[source==i]]
+                    inters = sub.intersection(poly.exterior)
+                    if largest:
+                        neighbors[i] = [inters.length.idxmax()]
+                    else:
+                        neighbors[i] = [inters.length.idxmin()]
+            else:
+                delete.append(i)
+        else:
+            neighbors[i]=[]
+
+    W = libpysal.weights.W(neighbors, silence_warnings=True)
+    return gdf.drop(delete).dissolve(W.component_labels)
+
--- geoplanar/tests/test_overlap.py
+++ geoplanar/tests/test_overlap.py
@@ -5,8 +5,7 @@ import numpy
 from numpy.testing import assert_equal
 from shapely.geometry import box
 
-from geoplanar.overlap import is_overlapping, trim_overlaps, merge_overlaps
-
+from geoplanar.overlap import is_overlapping, trim_overlaps, merge_overlaps, merge_touching
 
 class TestOverlap:
     def setup_method(self):
@@ -40,7 +39,7 @@ class TestOverlap:
 
         gdf1 = trim_overlaps(self.gdf2)
         assert_equal(gdf1.area.values, numpy.array([96.0, 96.0, 8.0]))
-
+        
     def test_merge_overlaps(self):
         gdf1 = merge_overlaps(self.gdf, 10, 0)
         assert_equal(gdf1.area.values, numpy.array([104]))
@@ -53,7 +52,30 @@ class TestOverlap:
 
         gdf1 = merge_overlaps(self.gdf, 1, 1)
         assert_equal(gdf1.area.values, numpy.array([100,8]))
-    
+        
     def test_merge_overlaps_multiple(self):
         gdf1 = merge_overlaps(self.gdf2, 10, 0)
         assert_equal(gdf1.area.values, numpy.array([200]))
+
+class TestTouching:
+    def setup_method(self):
+        self.p1 = box(0, 0, 1, 1)
+        self.p2 = box(1, 0, 11, 10)
+        self.p3 = box(15, 0, 25, 10)
+        self.p4 = box(0, 15, 1, 16)
+        self.p5 = box(0.5, 1, 1, 8)
+        self.gdf = geopandas.GeoDataFrame(geometry=[self.p1, self.p2, self.p3, self.p4, self.p5])
+        self.index = [0, 3]
+
+    def test_merge_touching_largest(self):
+        gdf1 = merge_touching(self.gdf,self.index,largest=True)
+        assert_equal(gdf1.area.values, numpy.array([101,100,3.5]))
+    
+    def test_merge_touching_smallest(self):
+        gdf2 = merge_touching(self.gdf,self.index,largest=False)
+        assert_equal(gdf2.area.values, numpy.array([4.5,100,100]))
+
+    def test_merge_touching_none(self):
+        gdf3 = merge_touching(self.gdf,self.index,largest=None)
+        assert_equal(gdf3.area.values, numpy.array([4.5,100,100]))
+
